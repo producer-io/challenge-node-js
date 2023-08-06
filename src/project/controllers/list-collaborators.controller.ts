b20f7b8a46f64d93083024aec7e55c30744bfbad
Mostafa1201 <mostafa1201@hotmail.com>
2023-08-06 06:28:51 +0300

    #fix - handle the case when project id is not found before listing collaborators

--- src/project/controllers/list-collaborators.controller.ts
+++ src/project/controllers/list-collaborators.controller.ts
@@ -2,9 +2,18 @@ import {Request, Response} from "express";
 import {ProjectRepository} from "../repositories/project.repository";
 
 export const listCollaboratorsController = async (req: Request, res: Response) => {
-    const { projectId } = req.params;
-    const project = ProjectRepository.findById(Number(projectId));
-    res.status(200).send({
-        collaborators: project.listCollaborators!()
-    });
+    try {
+        const { projectId } = req.params;
+        const project = ProjectRepository.findById(Number(projectId));
+        if(!project){
+            throw new Error('Project not found');
+        }
+        res.status(200).send({
+            collaborators: project.listCollaborators!()
+        });
+    } catch (error :any) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
 }
