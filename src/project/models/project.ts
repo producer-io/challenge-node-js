4064ddb0d482b167e4592eecd9aad9d39f36e4f3
Mostafa1201 <mostafa1201@hotmail.com>
2023-08-06 03:56:21 +0300

    #feat - Add methods for list collaborators and add collaborator in project model

--- src/project/models/project.ts
+++ src/project/models/project.ts
@@ -1,7 +1,20 @@
 export class Project {
     name: string;
 
-    constructor(name: string) {
+    constructor(name: string,private collaborators: string[] = []) {
         this.name = name;
+    }
+
+    addCollaborator(email: string): void{
+        if(this.collaborators.includes(email)){
+            throw new Error('Cannot add a collaborator with the same email twice');
+        }
+        else{
+            this.collaborators.push(email);
+        }
+    }
+
+    listCollaborators(){
+        return this.collaborators;
     }
 }
