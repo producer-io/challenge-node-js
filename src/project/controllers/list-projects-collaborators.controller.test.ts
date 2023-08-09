b2c773e9c3463cff81111f4895f289c9fb65a416
anas.hossam <softeng.anas@gmail.com>
2023-08-10 02:09:09 +0300

    test: create project collaborator controller

---
+++ src/project/controllers/list-projects-collaborators.controller.test.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from "express";
+import {listProjectCollaboratorsController} from "./list-projects-collaborators.controller";
+import {ProjectRepository} from "../repositories/project.repository";
+import {Project} from "../models/project";
+import {Collaborator} from "../models/project";
+
+describe('Controller::listProjectCollaborators', () => {
+    const email = 'test@mail.com';
+    beforeEach(() => {
+        ProjectRepository.projects = [];
+        const project1 = new Project('Project 1');
+        project1.addCollaborator({email});
+        ProjectRepository.save(project1);
+    });
+
+    it('should return a list collaborators of project', () => {
+        // Arrange
+        let collaborators: Collaborator[] = [];
+        const req = {params: {id: 0}} as any as Request
+        const res = {send: (_) => collaborators = _} as any as Response;
+
+        // Act
+        listProjectCollaboratorsController(req, res);
+
+        // Assert
+        expect(collaborators.length).toBe(1);
+        expect(collaborators[0].email).toBe(email);
+   });
+});
